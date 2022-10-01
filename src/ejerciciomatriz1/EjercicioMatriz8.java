cb273d2155641702cab0dd0447f296ce19006a31
xxgpwaxx <q00074f@upla.edu.pe>
2022-10-01 09:16:03 -0500

    Subir

--- src/ejerciciomatriz1/EjercicioMatriz8.java
+++ src/ejerciciomatriz1/EjercicioMatriz8.java
@@ -13,12 +13,15 @@ import java.util.Scanner;
 public class EjercicioMatriz8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int [][] matriz = new int[3][3];
-        int [] sumF = new int [3];       
+        int t = 4;
+        int [][] matriz = new int[t][t];
+        int [] sumF = new int [t]; 
         for(int i=0;i <matriz.length;i++){
             for(int j=0; j<matriz.length;j++){
-                System.out.println("Ingrese el valor de la posicion : ["+i+"]["+j+"] = ");
-                matriz[i][j] = sc.nextInt();
+                int aleat = (int) (Math.random()*20 + 1);
+                System.out.println("Ingrese el valor de la posicion : ["+i+"]["+j+"] = "+aleat);
+                matriz[i][j] = aleat;
+                //matriz[i][j] = sc.nextInt();
                 if(i % 2 == 0 && i != 0){
                     sumF[i] += matriz[i][j];
                 }
@@ -26,10 +29,10 @@ public class EjercicioMatriz8 {
         }
         for(int i=0; i < matriz.length;i++){
             System.out.println(Arrays.toString(matriz[i]));
-        }      
+        } 
         for(int i=0; i < sumF.length;i++){
             if(i % 2 == 0 && i != 0){
-                System.out.println("La suma de todas las filas pares "+(i)+" es : "+sumF[i]);
+                System.out.println("La suma de la Filas pares "+(i)+" es : "+sumF[i]);
             }
         }
     }
