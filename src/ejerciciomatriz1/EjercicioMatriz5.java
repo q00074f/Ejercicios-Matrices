b9dacfcd104e1a47065250595d5449c48207e580
xxgpwaxx <q00074f@upla.edu.pe>
2022-10-01 09:12:35 -0500

    Subir

--- src/ejerciciomatriz1/EjercicioMatriz5.java
+++ src/ejerciciomatriz1/EjercicioMatriz5.java
@@ -14,25 +14,25 @@ import java.util.Scanner;
 public class EjercicioMatriz5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = 3;
+        int t = 2;
         int [][] matriz = new int[t][t];
         int [] sumF = new int [t];
         for(int i=0;i <matriz.length;i++){
-        for(int j=0; j<matriz.length;j++){
-            System.out.println("Ingrese el valor de la posicion : ["+i+"]["+j+"] = ");
-            matriz[i][j] = sc.nextInt();
-            //matriz[i][j] = sc.nextInt();
-            if(j % 2 == 1){
-             sumF[j] += matriz[i][j];
-                          }
-                                         }
-                                         } 
+            for(int j=0; j<matriz.length;j++){
+                int aleat = (int) (Math.random()*20 + 1) ;
+                System.out.println("Ingrese el valor de la posicion : ["+i+"]["+j+"] = "+aleat);
+                matriz[i][j] = aleat;
+                if(j % 2 == 1){
+                    sumF[j] += matriz[i][j];
+                }
+            }
+        }
         for(int i=0; i < matriz.length;i++){
             System.out.println(Arrays.toString(matriz[i]));
         } 
         for(int i=0; i < sumF.length;i++){
             if(i % 2 == 1){
-                System.out.println("La suma de todas las columnas impares es : "+sumF[i]);
+                System.out.println("La suma de la Columna impar "+(i)+" es : "+sumF[i]);
             }
         }
     }
